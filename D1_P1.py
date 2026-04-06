6e267a57f5011b77f9e32099b7d8bd18417c1019
Mohammed Muzammil <muzammilknl1@gmail.com>
2026-04-06 22:13:38 +0530

    Initial commit

---
+++ D1_P1.py
@@ -0,0 +1,2 @@
+print("""This is from Pycharm""")
+print('Next line')
---
+++ D2_P2.py
@@ -0,0 +1,18 @@
+#Write a program to print your name using input function
+#1.Input function comes from built in module(default/predefine)
+# name1=input()
+# print(name1)
+# print(type(name1))
+
+
+#using input function
+#  My name is: Hafsa/Tahoor
+n2=input('Enter your name:')
+print('My name is:',n2)
+
+#I/P:Enter your name:Tahoora
+#o/p:My name is: Tahoora
+
+#WAP to find double of an integer
+#I/P:Enter any integer:2
+#O/P:Double of te no is:4
