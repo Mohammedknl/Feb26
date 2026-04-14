c98be192194a17a8d2061e5bf5ba44706a444c53
Mohammed Muzammil <muzammilknl1@gmail.com>
2026-04-14 21:50:19 +0530

    D2_P2 new line added

---
+++ D6-P2.py
@@ -0,0 +1,33 @@
+#Example prog on Arithmetic operators
+a=2+3
+print(a)
+print(type(a))
+b=3-2
+print(b)
+print(type(b))
+c=2-3
+print(c)
+print(type(c))
+d=3*2
+print(d)
+print(type(d))
+e=4/2
+print(e)
+print(type(e))
+f=5.0%2
+print(f)
+print(type(f))
+g=5//2.0
+print(g)
+print(type(g))
+h=2**3
+print(h)
+
+'''
+WAP to convert a given no of days taken as input to 
+a measure of time given in years,weeks and days
+I/P:Enter no of days:375
+O/P:375 days has 1 year,1 week and 3 days   using Arithmetic operators(+,_,*,/,%,//,**) 
+and print the result using placeholder method
+
+'''
---
+++ D6_P1.py
@@ -0,0 +1,4 @@
+a="Tahoora"  # passing a string value to a variable a
+b=25
+b=f"Hello from {a} and I am  {b} years old"
+print(b)
